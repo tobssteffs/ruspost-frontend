8b796ca797e89024773e3e67daa9eb0daced1e8c
RichStone <richard.had@hotmail.de>
2020-12-07 13:51:52 +0100

    Make sending request

--- sendContactForm.js
+++ sendContactForm.js
@@ -8,11 +8,25 @@
     const url = getRequestUrl();
 
     const formData = getSerializedJson(emailForm);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: formData,
+    })
+    const resJson = await response.json()
+    console.log('json',resJson)
   });
 
   function getRequestUrl() {
-    const localBaseUrl = 'http://localhost:5000/api/contact_form/';
-    const remoteBaseUrl = 'https://ruspost.herokuapp.com/api/contact_form/';
+    const localBaseUrl = 'http://localhost:5000/api/contact_form';
+    const remoteBaseUrl = 'https://ruspost.herokuapp.com/api/contact_form';
     const isLocalEnv = location.hostname === '' || location.hostname === 'localhost' || location.hostname === '127.0.0.1';
     const baseUrl = isLocalEnv ? localBaseUrl : remoteBaseUrl;
     return baseUrl;
