c8e0ee5f9a77b82dcd94170f5194462ff2b0ac82
RichStone <richard.had@hotmail.de>
2020-12-10 20:39:50 +0100

    Translate feedback in button & send language code with request

--- sendContactForm.js
+++ sendContactForm.js
@@ -1,15 +1,16 @@
 (async function homePage() {
   const emailForm = document.getElementById('email-form');
   const submitButton = document.getElementById('submit-contact-form');
+  const landuageCode = getLanguageCode();
 
   emailForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     emailForm.checkValidity();
 
     console.log('Start request');
-    submitButton.value = 'Wird gesendet...';
+    submitButton.value = translation('btnSendingForm', landuageCode);
     const url = getRequestUrl();
-    const formData = getSerializedJson(emailForm);
+    const formData = getSerializedFormDataJson();
     const response = await fetch(url, {
       method: 'POST',
       mode: 'cors',
@@ -23,10 +24,28 @@
     });
     const resJson = await response.json();
     console.log(resJson);
-    submitButton.value = 'Versandt!';
+    submitButton.value = translation('btnFormSent', landuageCode)
 
     displayFeedback(resJson);
   });
+
+  function getLanguageCode() {
+    // The website is structured with directories for languages in the first
+    // part of the URL path.
+    const languageDirectory = window.location.pathname.split('/').filter(pathPart => pathPart)[0];
+    // If no directory is given, then most probably we are at the
+    // homepage, which uses the default language German.
+    if (!languageDirectory) {
+      return 'de';
+    }
+    // If the resulting path part isn't included in the supported languages codes
+    // then most likely we are doing the request from local development.
+    if (!['en', 'de', 'ru'].includes(languageDirectory)) {
+      return 'en';
+    }
+
+    return languageDirectory;
+  }
 
   function getRequestUrl() {
     const localBaseUrl = 'http://localhost:5000/api/contact_form';
@@ -36,16 +55,23 @@
     return baseUrl;
   }
 
-  function getFormData() {
-    return new FormData(emailForm);
-  }
+  function getSerializedFormDataJson() {
+    const formData = new FormData(emailForm);
 
-  function getSerializedJson() {
-    const formData = getFormData();
+    // Add language code.
+    formData.append('lang-code', landuageCode)
+
+    if (formData.get('issue-type') === null) {
+      // Add issue-type manually to determine a target address since it's not
+      // provided in the form but we still need a recipient.
+      formData.append('issue-type', 'sales@');
+    }
+
     var object = {};
     formData.forEach(function(value, key){
         object[key] = value;
     });
+
     return JSON.stringify(object);
   }
 
@@ -66,5 +92,25 @@
       errorContainer.firstElementChild.innerHTML = response['message'];
       errorContainer.style.display = 'block';
     }
+  }
+
+  function translation(type, langCode) {
+    console.log('type', type)
+    console.log('code', langCode)
+
+    const translations = {
+      'btnSendingForm': {
+        'de': 'Wird versendet...',
+        'en': 'Sending form...',
+        'ru': 'Высылается...'
+      },
+      'btnFormSent': {
+        'de': 'Versandt!',
+        'en': 'Sent!',
+        'ru': 'Выслано!'
+      }
+    }
+
+    return translations[type][langCode];
   }
 })()
